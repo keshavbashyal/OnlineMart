ced21eab4e410e6f9df3b6a4b4acb90f8f2d7dec
PTamang <PTamang@cs_labv32-103.cs.mum.edu>
2015-01-05 14:31:36 -0600

    index title changed

--- src/main/java/com/onlinemart/model/ProductShoppingCart.java
+++ src/main/java/com/onlinemart/model/ProductShoppingCart.java
@@ -12,6 +12,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 
 /**
  *
@@ -25,7 +26,7 @@ public class ProductShoppingCart implements Serializable {
     private Long id;
     private int quantity;
     
-    @OneToMany
+    @OneToOne
     private Product product;
     
     public int getQuantity() {
